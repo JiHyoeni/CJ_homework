ed8f150294c8454febb34251d6afa3d3a8974f30
jihyeon <jihyoen0919@icloud.com>
2025-06-21 12:26:41 +0900

    - Add dismiss button

--- CJ_Homework/CJ_Homework/View/ProductDetail.swift
+++ CJ_Homework/CJ_Homework/View/ProductDetail.swift
@@ -13,10 +13,31 @@ struct ProductDetail: View {
     ///test data
 //    let product: Product = Product(id: "2058724538", name: "카시아 속초 스위트룸 주중 주말 균일가 2박", brand: "카시아 속초", price: 599000, discountPrice: 53910, discountRate: 10, image: "https://image.cjonstyle.net/goods_images/20/538/2058724538L.jpg", link: "https://item.cjonstyle.com/item/2058724538?channelCode=30002002", tags: ["방송상품"], benefits: [], rating: 3.5, reviewCount: 6)
     
+    @Environment(\.dismiss) private var dismiss
     var body: some View {
-        ProductWebView(url: product.link)
-            .navigationTitle(product.name)
-            .navigationBarTitleDisplayMode(.inline)
+        ZStack {
+            ProductWebView(url: product.link)
+                .navigationBarBackButtonHidden()
+            
+            ///닫기 버튼
+            VStack {
+                Spacer()    //하단 정렬
+                HStack {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Image(systemName: "arrowshape.backward.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .clipShape(Circle())
+                    }
+                    Spacer()    //좌측 왼쪽 정렬
+                }
+                .padding(.leading, 20)
+                .padding(.bottom, 20)
+                .foregroundColor(.gray)
+           }
+        }
     }
 }
 //struct ProductDetail_Preview: PreviewProvider {
