4d8634782a460ecac29b3faeeb33849c3363e67e
jihyeon <jihyoen0919@icloud.com>
2025-06-21 09:31:19 +0900

    - 프로젝트 생성

---
+++ CJ_Homework/CJ_Homework/CJ_HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CJ_HomeworkApp.swift
+//  CJ_Homework
+//
+//  Created by 박지현 on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct CJ_HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CJ_Homework/CJ_Homework/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CJ_Homework
+//
+//  Created by 박지현 on 6/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
